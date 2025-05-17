9627e3f9e1bc086aebc229accef197a1dcdbd536
shabaanyusuf <shabaanyusuf6@gmail.com>
2025-05-17 16:47:44 +0400

    creating cal v0.0

---
+++ src/cal.py
@@ -0,0 +1,24 @@
+def add (a,b)
+ return(a+b)
+ 
+ 
+ 
+ 
+ 
+ def sub (a,b)
+  return(a-b)
+ 
+ 
+ def mult (a,b)
+  return(a*b)
+ 
+ 
+ 
+ def dev (a/b)
+ if b==0:
+ 	return none;
+  return(a/b)
+ 
+ 
+ 
+ 
