e1e2531c6e68b8ad5ce3cf13f1777ed7ac896f36
shabaanyusuf <shabaanyusuf6@gmail.com>
2025-05-17 17:55:14 +0400

    third commit print func

--- src/cal.py
+++ src/cal.py
@@ -1,10 +1,6 @@
 def add (a,b)
  return(a+b)
  
- 
- 
- 
- 
  def sub (a,b)
   return(a-b)
  
@@ -12,12 +8,15 @@ def add (a,b)
  def mult (a,b)
   return(a*b)
  
- 
- 
  def dev (a/b)
  if b==0:
  	return none
   return(a/b)
+ 
+ print (add(2.4))
+ print (sub(2.4))
+ print (mult(2.4))
+ print (dev(2.4))
  
  
  
